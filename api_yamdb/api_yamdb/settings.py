e99a49b3e91e7f366b47bb4376821eb305b3eaf4
Alexandr Batanov <pokertrend@yandex.ru>
2023-05-14 21:58:58 +0400

    run

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default='default')
+SECRET_KEY = os.getenv('SECRET_KEY', default='default')
 
 DEBUG = False
 
